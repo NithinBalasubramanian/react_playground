1f2a39589bda9f280f7f0098e31c0557df76ce09
NithinBalasubramanian <nithinmigocrogle22@outlook.com>
2020-12-01 21:27:07 +0530

    Api setup

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET,POST");
+header("Access-Control-Allow-Headers:*");
+
 class Api extends CI_Controller{
 	public $CI = NULL;
 	function __construct(){
@@ -7,6 +11,36 @@ class Api extends CI_Controller{
 		$this->CI = & get_instance();
 	}
 	
-	
-
+	public function react_post()
+	{
+		$post_got = file_get_contents("php://input");
+		$data_got = json_decode($post_got);
+		$data = array(
+			'name' => $data_got->name,
+			'contact' => $data_got->contact,
+			'email' => $data_got->email,
+		);
+		$this->Api_model->create('react_customer',$data);
+    }
+    public function insert($tablename){
+        echo $tablename; exit;
+        $post_got = file_get_contents("php://input");
+		$data_got = json_decode($post_got);
+        $columns = $this->Admin_model->table($tablename);
+        for($i=0;$i<count($columns);$i++)
+        {
+            if($columns[$i]!="id")
+            {
+               if($columns[$i]=="date_created") {
+                    $date = date('Y-m-d');
+                    $data[$columns[$i]] = $date;
+                } else {
+                    $data[$columns[$i]] = $data_got->$columns[$i];
+                }
+            }
+        }
+        $insert = $this->Admin_model->create($tablename,$data);
+    }
+    
 }
+?>
