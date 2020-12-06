7e21ad0552f2bcaf76650a8ebcf3f3d21f4f9d06
NithinBalasubramanian <nithinmigocrogle22@outlook.com>
2020-12-06 14:54:22 +0530

    supplier validation

--- src/components/dashboard/addSupplierForm.js
+++ src/components/dashboard/addSupplierForm.js
@@ -1,18 +1,109 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './dashboard.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-function addSupplierForm() {
+function AddSupplierForm() {
+
+    const initialState = { 
+        s_name : '',
+        s_email : '',
+        s_contact : '',
+        s_sec_contact : '',
+        address : '',
+        nameError : '',
+        emailError : '',
+        contactError : '',
+        seccontactError : '',
+        disable : false,
+    };
+
+    const [ state , setState] = useState({ 
+        s_name : '',
+        s_email : '',
+        s_contact : '',
+        s_sec_contact : '',
+        address : '',
+        nameError : '',
+        emailError : '',
+        contactError : '',
+        seccontactError : '',
+        disable : false,
+    });
+
+    const onChangeHandler = (e) => {
+
+        let nowValue = e.target.value;
+        let nowName = e.target.name;
+        setState(prevstate => { 
+            return {...prevstate , [e.target.name] : nowValue }
+        });
+        verification(nowName , nowValue);
+    }
+
+    const verification = (name , value) => {
+        if(name === 's_name'){
+            if(value.length < 8){
+                setState( prevstate => {
+                    return { ...prevstate , nameError : '( Name strength must be above 8 )'}
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : true }
+                });
+            }else{
+                setState( prevstate => {
+                    return { ...prevstate , nameError : ''}
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : false }
+                });
+            }
+        }
+        if(name === 's_contact'){
+            if(value.length !== 10){
+                setState( prevstate => {
+                    return { ...prevstate , contactError : '( Contact Number is invalid )' }
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : true }
+                });
+            }else{
+                setState( prevstate => {
+                    return { ...prevstate , contactError : ''}
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : false }
+                });
+            }
+        }
+        if(name === 's_sec_contact'){
+            if(value.length !== 10){
+                setState( prevstate => {
+                    return { ...prevstate , seccontactError : '( Contact Number is invalid )' }
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : true }
+                });
+            }else{
+                setState( prevstate => {
+                    return { ...prevstate , seccontactError : ''}
+                });
+                setState( prevstate => {
+                    return { ...prevstate , disable : false }
+                });
+            }
+        }
+    }
 
     const formSubmitHandler = (e) => {
         e.preventDefault();
-        let data = {
-            id : 12
-        }
-        axios.post("http://localhost/react_play/Api/insert/supplier",data)
-        .then(response => { console.log(response) })
-        .catch(error => { console.log(error) })
+        // let data = {
+        //     id : 12
+        // }
+        // axios.post("http://localhost/react_play/Api/insert/supplier",data)
+        // .then(response => { console.log(response) })
+        // .catch(error => { console.log(error) })
+        setState( initialState );
     }
 
     return (
@@ -21,24 +112,43 @@ function addSupplierForm() {
                 <div className="col-md-12 ">
                     <form className="row" onSubmit={ formSubmitHandler }>
                         <div className="form-group col-md-6">
-                            <label>Supplier Name :* </label>
-                            <input type="text" className="form-control" name="s_name"></input>
+                            <label>Supplier Name :* </label><span><small style= {{ color:'red',padding:'opx 10px' }}>{ state.nameError }</small></span>
+                            <input type="text" 
+                            className={ (state.nameError !== '') ? `form-control is-invalid` : ` form-control` }  
+                            value={ state.s_name } 
+                            onChange={ onChangeHandler } 
+                             name="s_name"></input>
                         </div>
                         <div className="form-group col-md-6">
-                            <label>Supplier Email :* </label>
-                            <input type="email" className="form-control" name="s_email"></input>
+                            <label>Supplier Email :* </label><span><small style= {{ color:'red',padding:'opx 10px' }}>{ state.emailError }</small></span>
+                            <input type="email" 
+                            className={ (state.emailError !== '') ? `form-control is-invalid` : ` form-control` }  
+                            value={ state.s_email } 
+                            onChange={ onChangeHandler } 
+                            name="s_email"></input>
                         </div>
                         <div className="form-group col-md-6">
-                            <label>Supplier Contact :* </label>
-                            <input type="text" className="form-control" name="s_contact"></input>
+                            <label>Supplier Contact :* </label><span><small style= {{ color:'red',padding:'opx 10px' }}>{ state.contactError }</small></span>
+                            <input type="text" 
+                            className={ (state.contactError !== '') ? `form-control is-invalid` : ` form-control` }  
+                            value={ state.s_contact } 
+                            onChange={ onChangeHandler } 
+                            name="s_contact"></input>
                         </div>
                         <div className="form-group col-md-6">
-                            <label>Supplier Secondary Contact : </label>
-                            <input type="text" className="form-control" name="s_sec_cont"></input>
+                            <label>Supplier Secondary Contact : </label><span><small style= {{ color:'red',padding:'opx 10px' }}>{ state.seccontactError }</small></span>
+                            <input type="text" 
+                            className={ (state.seccontactError !== '') ? `form-control is-invalid` : ` form-control` }  
+                            value={ state.s_sec_contact } 
+                            onChange={ onChangeHandler } 
+                            name="s_sec_contact"></input>
                         </div>
                         <div className="form-group col-md-6">
                             <label>Address : </label>
-                            <textarea type="text" rows="5" className="form-control" name="s_address"></textarea>
+                            <textarea type="text" 
+                            rows="5" 
+                            className="form-control" 
+                            name="s_address"></textarea>
                         </div>
                         <div className="col-md-12">
                             <button type="submit" className="sideButton submitButton">Submit</button>
@@ -50,4 +160,4 @@ function addSupplierForm() {
     )
 }
 
-export default addSupplierForm
+export default AddSupplierForm
